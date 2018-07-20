8eeb49235cf0913b9ff66c6e4ef1f43830aebef5
Arjun Thandi <arjunthandi@gmail.com>
2018-07-20 20:26:30 +0100

    second test

--- test/TrueWordsHappyPath.js
+++ test/TrueWordsHappyPath.js
@@ -12,8 +12,11 @@ contract('TrueWords', function(accounts){
   var messageIsPublic1 = true;
   var messageRevealTime1 = 1513937410;
   var messageSubject2 = "message 2";
+  var messageSourceName2 = "Source name 2";
   var messageDescription2 = "Description for message 2";
   var messagePrice2 = 20;
+  var messageIsPublic2 = true;
+  var messageRevealTime2 = 1513937410;
   var sourceBalanceBeforeBuy, ssourceBalanceAfterBuy;
   var revealToBalanceBeforeBuy, revealToAfterBuy;
 
@@ -60,6 +63,41 @@ contract('TrueWords', function(accounts){
       assert.equal(data[0].toNumber(), 1, "Message id must be 1");
       assert.equal(data[3], messageSubject1, "message name must be " + messageSubject1);
       assert.equal(data[4], messageDescription1, "Message description must be " + messageDescription1);
+    });
+  });
+
+  // sell a second message
+  it("should let us post a second message", function() {
+    return TrueWords.deployed().then(function(instance){
+      trueWordsInstance = instance;
+      return trueWordsInstance.postMessage(
+        messageSourceName2,
+        messageSubject2,
+        messageDescription2,
+        messageIsPublic2,
+        messageRevealTime2
+      );
+    }).then(function(receipt){
+      // check event
+      assert.equal(receipt.logs.length, 1, "one event should have been triggered");
+      assert.equal(receipt.logs[0].event, "LogPostMessage", "event should be LogPostMessage");
+      assert.equal(receipt.logs[0].args._id.toNumber(), 2, "id must be 2");
+      assert.equal(receipt.logs[0].args._subject, messageSubject2, "event subject name must be " + messageSubject2);
+
+      return trueWordsInstance.getNumberOfMessages();
+    }).then(function(data) {
+      assert.equal(data, 2, "number of messages must be two");
+
+      return trueWordsInstance.getPublicMessages();
+    }).then(function(data) {
+      assert.equal(data.length, 2, "there must be two messages for sale");
+      assert.equal(data[1].toNumber(), 2, "message id must be 2");
+
+      return trueWordsInstance.messages(data[1]);
+    }).then(function(data) {
+      assert.equal(data[0].toNumber(), 2, "message id must be 2");
+      assert.equal(data[3], messageSubject2, "message name must be " + messageSubject2);
+      assert.equal(data[4], messageDescription2, "message description must be " + messageDescription2);
     });
   });
 });
