eab7cafb5fce36afa88bf770511c55c2f3a44684
Arjun Thandi <arjunthandi@gmail.com>
2018-08-14 19:54:09 +0100

    added network for metamask to be able to connect to

--- truffle.js
+++ truffle.js
@@ -6,6 +6,13 @@ module.exports = {
                host: "localhost",
                port: 7545,
                network_id: "*" // Match any network id
+          },
+          truewords: {
+               host: "localhost",
+               port: 8545,
+               network_id: "4224",
+               gas: 4700000,
+               from: '0x5dae6dbb4c07a3d7ec7705bc642f7027b7c8534c'
           }
      }
 };
